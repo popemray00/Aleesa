79b029884ee73f2fc1fe711d9da7f7ed9d457d1d
popemray00 <popemray1986@gmail.com>
2021-06-16 14:14:29 -0700

    main img added

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import { Parallax } from "react-parallax";
 import { FaInstagram } from "react-icons/fa";
+import { BsCaretDownFill } from "react-icons/bs";
 
 const image1 =
     "https://pixnio.com/free-images/2017/02/25/2017-02-25-06-48-04.jpg"
@@ -22,13 +23,31 @@ class Home extends React.Component {
           </div>
           
           <div className="mainbody">
+            <img src="wifey2.png" className="mainimg"></img>
             <h1>My Commitment to you!</h1>
-            <p>Is to be the best</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
+
+Quam elementum pulvinar etiam non quam. Ultrices eros in cursus turpis massa tincidunt dui. Tincidunt augue interdum velit euismod in. Suspendisse potenti nullam ac tortor vitae purus. Duis tristique sollicitudin nibh sit amet commodo nulla. Dolor sit amet consectetur adipiscing. Risus nullam eget felis eget nunc. Viverra tellus in hac habitasse. Integer eget aliquet nibh praesent tristique magna sit amet. Ultrices dui sapien eget mi. Arcu felis bibendum ut tristique et egestas. Malesuada pellentesque elit eget gravida cum sociis natoque. Sem integer vitae justo eget. Placerat vestibulum lectus mauris ultrices. Elit pellentesque habitant morbi tristique.</p>
           </div>
 
           <div className="social">
-          <a href="https://www.instagram.com/hair_by_aleesa/" target="blank"><FaInstagram size="5em" className="Instagram" /></a>
-            
+            <h1>Check out my work!</h1>
+            <BsCaretDownFill size="4em" className="arrow"/>
+              <br></br>
+            <BsCaretDownFill size="4em" className="arrow"/>
+              <br></br>
+            <BsCaretDownFill size="4em"className="arrow"/>
+              <br></br>
+            <a href="https://www.instagram.com/hair_by_aleesa/" target="blank"><FaInstagram size="10em" className="Instagram" /></a>
+          </div>
+
+          <div className="footer">
+            <h1>Contact Me...</h1>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
           </div>
         </div>
     )
