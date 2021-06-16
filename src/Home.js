5351b37a645067e7193b056cfeeb1eaa62479d6a
popemray00 <popemray1986@gmail.com>
2021-06-16 14:44:58 -0700

    parallax 2 added

--- src/Home.js
+++ src/Home.js
@@ -22,13 +22,15 @@ class Home extends React.Component {
             <h1>"Life is better when you have a good hairstylist"</h1>
           </div>
           
-          <div className="mainbody">
-            <img src="wifey2.png" className="mainimg"></img>
-            <h1>My Commitment to you!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
-
-Quam elementum pulvinar etiam non quam. Ultrices eros in cursus turpis massa tincidunt dui. Tincidunt augue interdum velit euismod in. Suspendisse potenti nullam ac tortor vitae purus. Duis tristique sollicitudin nibh sit amet commodo nulla. Dolor sit amet consectetur adipiscing. Risus nullam eget felis eget nunc. Viverra tellus in hac habitasse. Integer eget aliquet nibh praesent tristique magna sit amet. Ultrices dui sapien eget mi. Arcu felis bibendum ut tristique et egestas. Malesuada pellentesque elit eget gravida cum sociis natoque. Sem integer vitae justo eget. Placerat vestibulum lectus mauris ultrices. Elit pellentesque habitant morbi tristique.</p>
-          </div>
+          <Parallax className="para2" bgImage="wifey4.jpg" strength={400}>
+            <div style={{ height: 800 }}>
+              <div className="textbox2">
+                <h1>My Commitment to you!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
+Quam elementum pulvinar etiam non quam.</p>
+              </div>
+            </div>
+          </Parallax>
 
           <div className="social">
             <h1>Check out my work!</h1>
