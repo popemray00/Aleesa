955b80d115d3e0f3b15db969539b0f1406e978ef
popemray00 <popemray1986@gmail.com>
2021-07-11 14:54:24 -0700

    carousel added

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import { Parallax } from "react-parallax";
 import { FaInstagram } from "react-icons/fa";
 import { BsCaretDownFill } from "react-icons/bs";
+import Carousel from 'react-bootstrap/Carousel'
 
 const image1 =
     "https://pixnio.com/free-images/2017/02/25/2017-02-25-06-48-04.jpg"
@@ -22,18 +23,55 @@ class Home extends React.Component {
             <h1>"Life is better when you have a good hairstylist"</h1>
           </div>
           
-          <Parallax className="para2" bgImage="wifey4.jpg" strength={400}>
-            <div style={{ height: 800 }}>
-              <div className="textbox2">
-                <h1>My Commitment to you!</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
-Quam elementum pulvinar etiam non quam.</p>
-              </div>
-            </div>
-          </Parallax>
+          <div className="mainbody">
+            <img src="/wifey5.jpg" alt="pic of me" className="mainimg"></img>
+            <h1>My Commitment to you!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
 
-          <div className="social">
-            <h1>Check out my work!</h1>
+Quam elementum pulvinar etiam non quam. Ultrices eros in cursus turpis massa tincidunt dui. Tincidunt augue interdum velit euismod in. Suspendisse potenti nullam ac tortor vitae purus. Duis tristique sollicitudin nibh sit amet commodo nulla. Dolor sit amet consectetur adipiscing. Risus nullam eget felis eget nunc. Viverra tellus in hac habitasse. Integer eget aliquet nibh praesent tristique magna sit amet. Ultrices dui sapien eget mi. Arcu felis bibendum ut tristique et egestas. Malesuada pellentesque elit eget gravida cum sociis natoque. Sem integer vitae justo eget. Placerat vestibulum lectus mauris ultrices. Elit pellentesque habitant morbi tristique.</p>
+          </div>
+
+          <div className="work">
+          <Carousel>
+         <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="wifey4.jpg"
+              alt="First slide"
+            />
+         </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="wifey4.jpg"
+              alt="Sixth slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="wifey4.jpg"
+              alt="Seventh slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="wifey4.jpg"
+              alt="Eight slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="wifey4.jpg"
+              alt="Ninth slide"
+            />
+        </Carousel.Item>
+        </Carousel>
+
+          
+            <h1>Check Out More Work!</h1>
             <BsCaretDownFill size="4em" className="arrow"/>
               <br></br>
             <BsCaretDownFill size="4em" className="arrow"/>
