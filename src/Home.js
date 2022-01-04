c5c833ca189b9a6496d8fab381a8b54a2c10c20b
popemray00 <popemray1986@gmail.com>
2022-01-03 22:59:06 -0500

    About me complete

--- src/Home.js
+++ src/Home.js
@@ -20,9 +20,12 @@ class Home extends React.Component {
           
           <div className="mainbody">
             <img src="/wifey5.jpg" alt="pic of me" className="mainimg"></img>
-            <h1>My Commitment to you!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh praesent tristique magna sit amet purus gravida. Dictum non consectetur a erat nam. Quis imperdiet massa tincidunt nunc pulvinar sapien. Sociis natoque penatibus et magnis dis parturient montes nascetur. Gravida quis blandit turpis cursus in hac habitasse platea. Risus nec feugiat in fermentum. Tristique risus nec feugiat in fermentum posuere urna nec. Pellentesque id nibh tortor id aliquet lectus proin nibh nisl. Nulla pharetra diam sit amet nisl suscipit. A arcu cursus vitae congue. Hac habitasse platea dictumst quisque. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Libero nunc consequat interdum varius sit amet mattis vulputate enim. Ullamcorper eget nulla facilisi etiam. Etiam sit amet nisl purus in mollis. Fermentum dui faucibus in ornare quam viverra orci sagittis.
-               Quam elementum pulvinar etiam non quam. Ultrices eros in cursus turpis massa tincidunt dui. Tincidunt augue interdum velit euismod in. Suspendisse potenti nullam ac tortor vitae purus. Duis tristique sollicitudin nibh sit amet commodo nulla. Dolor sit amet consectetur adipiscing. Risus nullam eget felis eget nunc. Viverra tellus in hac habitasse. Integer eget aliquet nibh praesent tristique magna sit amet. Ultrices dui sapien eget mi. Arcu felis bibendum ut tristique et egestas. Malesuada pellentesque elit eget gravida cum sociis natoque. Sem integer vitae justo eget. Placerat vestibulum lectus mauris ultrices. Elit pellentesque habitant morbi tristique.</p>
+            <h1>About Me</h1><br></br>
+            <p id="commitment">Originally from Oregon, now residing in St John's Florida, I have worked as a professional in the beauty industry since I graduated Portland Beauty School in 2014. My specialties include and are not limited to, hair color, balayage, and men’s cuts.<br></br><br></br>
+            I enjoy furthering my education with as many classes as I can attend. I'm dedicated to keeping up with the latest styles and trends in the world of fashion and design, so that I can create any look my client desires. <br></br><br></br>
+            I enjoy a personal and caring relationship with all my clients and my goal is for each of my guest to feel pampered and relaxed during their appointment.<br></br><br></br>
+            To see more of my work, check out my <a href="https://www.instagram.com/hair_by_aleesa/" target="blank">Instagram</a>!
+            </p>
           </div>
 
           <div className="work">
