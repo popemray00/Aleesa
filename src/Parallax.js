a32793cb84c74afad4e430e1eb8500ccf16edb3d
popemray00 <popemray1986@gmail.com>
2022-01-05 16:40:55 -0500

    responsive css moved for large screens

--- src/Parallax.js
+++ src/Parallax.js
@@ -7,7 +7,7 @@ import { Parallax } from "react-parallax";
 const ParaHeader = () => {
     return (
         <Parallax bgImage={"aleesaheader.jpg"} strength={400}>
-            <div style={{ height: 900 }}>
+            <div style={{ height: 930 }}>
               <div className="textbox">Hair By <span id="name">Aleesa</span></div>
             </div>
           </Parallax>
