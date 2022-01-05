ef3477df83ac38ea0778bf97b00b4b5007babd6d
popemray00 <popemray1986@gmail.com>
2022-01-04 21:56:56 -0500

    css styling complete

--- src/Home.js
+++ src/Home.js
@@ -17,18 +17,22 @@ class Home extends React.Component {
           
           <div className="quote">
             <h2>"Life is better when you have a good hairstylist"</h2>
-            <div className="mainbody">
-            <img src="/wifey5.jpg" alt="pic of me" className="mainimg"></img>
-            <h1>About Me</h1><br></br>
-            <p>Originally from Oregon, now residing in St John's Florida, I have worked as a professional in the beauty industry since graduating from Portland Beauty School in 2014.<br></br><br></br> My specialties include and are not limited to, hair color, balayage, and men’s cuts.<br></br><br></br>
-            I'm continuing to further my education by taking as many classes as I can attend. I'm dedicated to keeping up with the latest styles and trends in the world of fashion and design. That way I can create any look my client desires.<br></br><br></br>
-            I enjoy a personal and caring relationship with all my clients, my goal is for each of my guest to feel pampered and relaxed during their appointment.<br></br><br></br>
-            To see more of my work, check out my <a href="https://www.instagram.com/hair_by_aleesa/" target="blank">Instagram</a>!
-            </p>
-          </div>
           </div>
 
+          <div className="mainbody">
+            <img src="/wifey5.jpg" alt="pic of me" className="mainimg"></img>
+              <h1>About Me</h1><br></br>
+                <p>Originally from Oregon, now residing in St John's Florida, I have worked as a professional in the beauty industry since graduating from Portland Beauty School in 2014.<br></br><br></br> My specialties include and are not limited to, hair color, balayage, and men’s cuts.<br></br><br></br>
+                  I continue to further my education by taking as many classes as I can attend. I'm dedicated to keeping up with the latest styles and trends in the world of fashion and design. That way I can create any look my client desires.<br></br><br></br>
+                  I enjoy a personal and caring relationship with all my clients, my goal is for each of my guest to feel pampered and relaxed during their appointment.<br></br><br></br>
+                  To see more of my work, check out my <a href="https://www.instagram.com/hair_by_aleesa/" target="blank">Instagram</a>!
+                </p>
+          </div>
+
+
+
           <div className="work">
+            <h1>My Work</h1>
           <Carouselslide />
             <a href="https://www.instagram.com/hair_by_aleesa/" target="blank"><FaInstagram size="10em" className="Instagram" /></a>
           </div>
